c3e431b3b6be05c80a89d889f41097aa76f5edf2
vince.pii <vinc.pii@gmail.com>
2010-06-01 17:09:14 +0200

    Gestione lock sul pagamento

--- pagamento.php
+++ pagamento.php
@@ -54,33 +54,68 @@ include_once('login.php');
 				die ('Invalid query: ' . mysql_error());
 
 			//invia una pagina di errore all'utente
-			echo "<h3>La sua prenotazione è scaduta. Il pagamento con la
+			echo "<h3>La sua prenotazione è scaduta o inesistente. Il pagamento con la
 					banca non è avvenuto.</h3>";		
 		}
 		else{
+			//salvataggio della prenotazione e cancellazione (ripristinata se pagamento non verrà fatto)
+			$salva_pezzi = $row['pezzi'];
+			$salva_scandenza = $row['scadenza'];
+			
+			$query = "DELETE FROM negozio.prenotazioni
+					WHERE prod_id = ".$id_prod." AND user_id = '".$_SESSION['user']."'";
+			$result = mysql_query($query, $link);
+			if (!$result)
+				die ('Invalid query: ' . mysql_error());
+				
+			$query = "UNLOCK TABLES;";
+			$result = mysql_query($query, $link);
+			if (!$result)
+				die ('Invalid query: ' . mysql_error());
+			
 			//transazione con la banca:
 			//la banca risponde in 10 secondi inviando l'esito del pagamento
 			sleep(rand(1,10));
 			//esito pagamento random
 			$esito = rand(0,1);
 			if ($esito){
-				//se banca dice OK: elimina prenotazione; unlock; messaggio all'utente
-				$query = "DELETE FROM negozio.prenotazioni
-				  			WHERE prod_id = ".$id_prod." AND user_id = '".$_SESSION['user']."';";
-
-				$result = mysql_query($query, $link);
-				if (!$result)
-					die ('Invalid query: ' . mysql_error());
-
-				$query = "UNLOCK TABLES;";
-				$result = mysql_query($query, $link);
-				if (!$result)
-					die ('Invalid query: ' . mysql_error());
-
+				//pagamento completato
 				echo "<h3>Pagamento eseguito con successo.</h3>";
 			}
 			else{
-				//se banca dice NO: unlock; messaggio all'utente
+				//pagamento non effettuato: ripristino della prenotazione
+				$query = "LOCK TABLES negozio.prenotazioni WRITE;";
+				$result = mysql_query($query, $link);
+				if (!$result)
+					die ('Invalid query: ' . mysql_error());
+					
+				//se nel frattempo è stata rifatta una prenotazione per lo stesso prodotto
+				//si deve aggiornare, altrimenti creare una nuova entry
+				$query = "SELECT * FROM negozio.prenotazioni
+						  WHERE prod_id = ".$id_prod." AND user_id = '".$_SESSION['user']."';";
+				$result = mysql_query($query, $link);
+				if (!$result)
+					die ('Invalid query: ' . mysql_error());
+				
+				$row = mysql_fetch_assoc($result);
+				if (!$row) {
+					//la stessa prenotazione non è stata rifatta
+					$query = "INSERT INTO negozio.prenotazioni
+							values(".$id_prod.", '".$_SESSION['user']."', ".$salva_pezzi.", '".$salva_scandenza."');";
+					$result = mysql_query($query, $link);
+					if (!$result)
+						die ('Invalid query: ' . mysql_error());
+				}
+				else {
+					//durante il pagamento è stata rifatta la stessa prenotazione
+					$query = "UPDATE negozio.prenotazioni
+							SET pezzi = pezzi + ".$salva_pezzi." 
+							WHERE prod_id = ".$id_prod." and user_id = '".$_SESSION['user']."'";
+					$result = mysql_query($query, $link);
+					if (!$result)
+						die ('Invalid query: ' . mysql_error());
+				}
+				
 				$query = "UNLOCK TABLES;";
 				$result = mysql_query($query, $link);
 				if (!$result)
